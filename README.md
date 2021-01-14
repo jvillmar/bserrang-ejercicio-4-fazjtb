ed740774bc4318ed2420d4f93ee3cdcf9e72e829
jvillmar <juan.carlos.villegas.marmol@everis.com>
2021-01-14 15:58:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bserrang-ejercicio-4-fazjtb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bserrang-ejercicio-4-fazjtb)
---
+++ script.js
@@ -0,0 +1,20 @@
+// En este ejercicio tenéis que sustituir TODAS las funciones tradicionales por funciones flecha y que siga funcionando igual.
+
+const changeBackgroundButton = document.getElementById(
+  "changeBackgroundButton"
+);
+
+changeBackgroundButton.addEventListener("click", ()=> {
+  changeBackgroundButton.style.backgroundColor = getRandomColor();
+});
+
+ getRandomColor = () => {
+  const letters = getLetters();
+  let color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+getLetters = () => '0123456789ABCDEF';
